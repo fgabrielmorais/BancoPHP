834f2990a152227de05b668c6d600c951a8df74c
fgabrielmorais <gabrielgamer.aleatoriow@gmail.com>
2023-05-25 07:44:24 -0300

    tudo feito

--- conexao.php
+++ conexao.php
@@ -1,11 +1,13 @@
 <?php
 
-$servidor =  "localhost";
-$banco = "";
-$usuario = "root";
-$senha = "devisate";
+$servidor =  "107.161.183.203";
+$banco = "terceirods_gabrielfelix";
+$usuario = "terceirods_gabriel_felix";
+$senha = "xXGMF_2005";
 
+$con = mysqli_connect($servidor, $usuario, $senha);
 
-
-
+    if(!$con){
+        echo "Erro ao conectar o banco de dados.";
+    }
 ?>
